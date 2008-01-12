c9578df5020931f5c9560c948e80dec8769ac81a
Lennart Poettering <lennart@poettering.net>
2008-01-12 23:51:45 +0000

    explicitly include libintl.h for FreeBSD compat. Problem spotted by  Romain Tartière
    git-svn-id: file:///home/lennart/svn/public/paprefs/trunk@35 54a58c19-9419-0410-9bbb-d8fa3e5d54ff

--- src/paprefs.cc
+++ src/paprefs.cc
@@ -28,6 +28,7 @@
 #include <gtkmm.h>
 #include <libglademm.h>
 #include <gconfmm.h>
+#include <libintl.h>
 
 #define PA_GCONF_ROOT "/system/pulseaudio"
 #define PA_GCONF_PATH_MODULES PA_GCONF_ROOT"/modules"
